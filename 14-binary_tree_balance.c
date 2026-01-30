87042cc60630fba653352b8ed66788b566636fda
Ilknur <ilknurkazimov308@gmail.com>
2026-01-30 21:53:48 +0400

    Add 14-binary_tree_balance.c code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,24 +10,24 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t lHeight, rHeight;
+	size_t lHeight, rHeight;
 
-    if (tree == NULL)
-        return (0);
-    
-    lHeight = binary_tree_height(tree->left);
-    rHeight = binary_tree_height(tree->right);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->parent == NULL)
-    {
-        if (lHeight > rHeight)
-            return (lHeight);
-        return (rHeight);
-    }
+	lHeight = binary_tree_height(tree->left);
+	rHeight = binary_tree_height(tree->right);
 
-    if (lHeight > rHeight)
-        return (lHeight + 1);
-    return (rHeight + 1);
+	if (tree->parent == NULL)
+	{
+		if (lHeight > rHeight)
+			return (lHeight);
+		return (rHeight);
+	}
+
+	if (lHeight > rHeight)
+		return (lHeight + 1);
+	return (rHeight + 1);
 }
 
 /**
@@ -38,8 +38,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
