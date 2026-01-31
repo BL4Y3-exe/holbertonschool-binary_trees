a10a64d732edf3c8831a0820efe72e911e943a74
Ilknur <ilknurkazimov308@gmail.com>
2026-01-31 19:09:03 +0400

    Add 17-binary_tree_sibling.c code

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -5,7 +5,7 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to a node
- * 
+ *
  * Return: pointer to the sibling node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
