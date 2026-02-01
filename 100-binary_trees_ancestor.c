9d8f46d2b816d70cece2f5d34ba0c16e3e99b17c
Ilknur <ilknurkazimov308@gmail.com>
2026-02-01 17:24:03 +0400

    Removed mixed declaration

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,22 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ *
+ * Return: pointer to the lowest common ancestor node
+ */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-    if (first == NULL || second == NULL)
-        return (NULL);
-    
-    const binary_tree_t *pfirst = first;
-    while (pfirst != NULL)
-    {
-        const binary_tree_t *psecond = second;
-        while (psecond != NULL)
-        {
-            if (pfirst == psecond)
-                return ((binary_tree_t *)pfirst);
-            psecond = psecond->parent;
-        }
-        pfirst = pfirst->parent;
-    }
-    return (NULL);
+	const binary_tree_t *pfirst = first;
+	const binary_tree_t *psecond = second;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	while (pfirst != NULL)
+	{
+		while (psecond != NULL)
+		{
+			if (pfirst == psecond)
+				return ((binary_tree_t *)pfirst);
+			psecond = psecond->parent;
+		}
+		pfirst = pfirst->parent;
+	}
+	return (NULL);
 }
